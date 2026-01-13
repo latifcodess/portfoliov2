5828616b9ee3fe2e3e3cf7303ce25ee33fef91fe
latifcodess <196327957+latifcodess@users.noreply.github.com>
2026-01-13 20:44:06 +0100

    chore: added files

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script>
+  export default{
+    name: 'App'
+  }
+</script>
+
+<template>
+  <div>
+    <h1>Latif.codes</h1>
+    <p>Jeune developpeur - ETML</p>
+  </div>
+</template>
+
+<style>
+  body{
+    font-family: 'JetBrains Mono', monospace;
+  }
+</style>
